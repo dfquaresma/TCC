20480b3eb9444bd4b596999221b2200ddb59eb61
dfquaresma <david.quaresma@ccc.ufcg.edu.br>
2021-05-03 19:49:19 -0300

    Update workload and add more measurement results

--- experiment/scripts/workload_run.sh
+++ experiment/scripts/workload_run.sh
@@ -2,10 +2,9 @@
 date
 set -x
 
-echo "TARGET: ${TARGET:=https://5vjlxy2vi2.execute-api.us-east-2.amazonaws.com/thumbnailator/ 
-measurement02.csv}"
+echo "TARGET: ${TARGET:=https://5vjlxy2vi2.execute-api.us-east-2.amazonaws.com/thumbnailator/}"
 echo "EXPI_ID: ${EXPI_ID:=measurement00.csv}"
-echo "RESULTS_PATH: ${RESULTS_PATH:=../../results/measurements}"
+echo "RESULTS_PATH: ${RESULTS_PATH:=/home/david/TCC/TCC/results/measurements/}"
 echo "NUMBER_OF_REQS: ${NUMBER_OF_REQS:=20000}"
 echo "LAMBDA: ${LAMBDA:=200}"
 
