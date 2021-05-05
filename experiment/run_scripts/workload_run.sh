792f929c3b57a7e4b5c353cc4cb55b573c1b7a79
dfquaresma <david.quaresma@ccc.ufcg.edu.br>
2021-05-05 14:21:35 -0300

    Update analysis and run scripts

--- experiment/run_scripts/workload_run.sh
+++ experiment/run_scripts/workload_run.sh
@@ -3,9 +3,10 @@ date
 set -x
 
 echo "TARGET: ${TARGET:=https://5vjlxy2vi2.execute-api.us-east-2.amazonaws.com/thumbnailator/}"
-echo "EXPI_ID: ${EXPI_ID:=measurement00.csv}"
+echo "EXPI_ID: ${EXPI_ID:=00}"
 echo "RESULTS_PATH: ${RESULTS_PATH:=/home/david/TCC/TCC/results/measurements/}"
 echo "NUMBER_OF_REQS: ${NUMBER_OF_REQS:=20000}"
 echo "LAMBDA: ${LAMBDA:=200}"
+echo "TYPE: ${TYPE:=measurement}"
 
-go run ../workload/main.go --target=${TARGET} --exp_id=${EXPI_ID} --results_path=${RESULTS_PATH} --nreqs=${NUMBER_OF_REQS} --lambda=${LAMBDA}
+go run ../workload/main.go --target=${TARGET} --exp_id="${TYPE}-lambda${LAMBDA}-${EXPI_ID}.csv" --results_path=${RESULTS_PATH} --nreqs=${NUMBER_OF_REQS} --lambda=${LAMBDA}
